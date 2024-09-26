a689b6397f6c14fa049106b58ff2e222fa454874
Luka <igorvelic7@gmail.com>
2024-09-26 10:09:44 -0700

    kolobaracija na projektu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Auto Centar Polis</h1>;
+	return <h1>Auto Centar Polis</h1>;
+	<h2>Luka test</h2>;
 }
 
 export default App;
